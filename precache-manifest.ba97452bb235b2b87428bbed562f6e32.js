40a28093123054cff9554920f1528fefd477118a
JacobHill60 <jacobdhill60@gmail.com>
2020-06-09 17:10:49 -0400

    Updates

---
+++ precache-manifest.ba97452bb235b2b87428bbed562f6e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afb1d18a73683e95d6bb18f0b190040",
+    "url": "/Psuedo-Youtube/index.html"
+  },
+  {
+    "revision": "4be34a96ad68afc1e9a9",
+    "url": "/Psuedo-Youtube/static/css/main.f574dabb.chunk.css"
+  },
+  {
+    "revision": "799dfac39e0ba81f899e",
+    "url": "/Psuedo-Youtube/static/js/2.36989d81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Psuedo-Youtube/static/js/2.36989d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be34a96ad68afc1e9a9",
+    "url": "/Psuedo-Youtube/static/js/main.5dc8de35.chunk.js"
+  },
+  {
+    "revision": "5859f6dd1e8e525c6028",
+    "url": "/Psuedo-Youtube/static/js/runtime-main.8b669908.js"
+  }
+]);
